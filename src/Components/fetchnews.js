51182e606af784fa29a08c9ac5da9e707ecb8c4a
Pratik Dhumal <dhumalpratik8421@gmail.com>
2022-05-28 07:05:45 +0530

    first commit

---
+++ src/Components/fetchnews.js
@@ -0,0 +1,45 @@
+import React,{useState} from 'react'
+import axios   from 'axios'
+
+
+function Fetchnews(){
+
+  const [news,setNews] = useState([])
+
+
+  const fetchNews=()=>{
+    axios.get(" https://newsapi.org/v2/top-headlines?country=in&apiKey=6d7101e514434a709b6cf5f31ec3565a")
+    .then((response)=>{
+           console.log(response);
+        setNews(response.data.articles )
+    })
+  }
+  return (
+    <div>
+     <button className='btn btn-primary' onClick={fetchNews}>Fetch news</button>
+
+   <div className="container">
+     <div className="row">
+       {
+         news.map((value )=>{
+           return(
+            <div className="col-4">
+       <div className="card" style={{width: "18rem"}}>
+  <img src={value.urlToImage} className="card-img-top" alt="... "/>
+  <div className="card-body">
+    <h5 className="card-title">{value.title}</h5>
+    <p className="card-text">{value.description}</p>
+    <a href="#" className="btn btn-primary">Main</a>
+  </div>
+</div> 
+       </div>  
+           )
+         })
+       }
+     </div>
+   </div>
+    </div>
+  )
+}
+
+export default Fetchnews;
